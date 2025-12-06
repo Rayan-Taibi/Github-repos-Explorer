71f1a5b4531955538535616dfc57495d6139122b
Rayane TAIBI <taibirayane86@gmail.com>
2025-12-06 08:53:34 +0100

    Reset username after fetching repositories and enhance Search component layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ function App() {
     }finally{
       setLoading(false);
     }
+    setUsername('');
     
   }
   return (
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,24 +5,24 @@ export default function Search({username, setUsername, fetchRepos,loading}) {
     <>
      <div className="max-w-6xl mx-auto px-4 py-6">
       <div className='mb-16 mt-16' >
-        <div className="bg-[#A3E635] border-4 border-black px-6 py-12 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]">
+        <div className="bg-[#A3E635] border-4 border-black px-6 py-12 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] md:">
             <h2 className='text-4xl md:text-5xl font-bold '>FIND USER</h2>
-            <div className='flex gap-4 mt-4'>
+          <div className='flex flex-col gap-4 mt-4 md:flex-row'> 
             <input 
             type="text"
             value={username}
             onKeyPress={(e) =>e.key === 'Enter' && fetchRepos() }
             onChange={(e) => setUsername(e.target.value)}
              placeholder="USER NAME HERE ..." 
-             className="flex-1 h-14 md:h-16 px-4 border-4 border-black bg-white text-lg md:text-xl font-bold placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#FF00FF] transition-all"
+             className="w-full h-16 bg-white border-4 border-black px-4 text-xl font-bold placeholder-gray-400 focus:outline-none focus:ring-4 focus:ring-[#FF90E8] transition-all"
              />
            <button
            onClick={fetchRepos}
            disabled={loading}
-            className="h-14 md:h-16 px-6 md:px-8 bg-black text-white font-bold text-lg md:text-2xl border-4 border-black hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:bg-white hover:text-black transition-all whitespace-nowrap active:translate-y-1 active:translate-x-1 hover:cursor-pointer">
-             SEARCH USER
+            className="h-16 px-8 bg-black text-white font-black text-xl border-4 border-transparent hover:bg-white hover:text-black hover:border-black transition-all active:translate-y-1 active:translate-x-1 disabled:opacity-50 disabled:cursor-not-allowed shadow-[4px_4px_0px_0px_rgba(255,255,255,0.5)] cursor-pointer">
+             { loading ? 'SEARCHING...' : 'SEARCH_DB' }
            </button>
-           </div>
+          </div>
         </div>
       </div>
     </div>
